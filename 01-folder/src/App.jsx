a22d3bd7c916328f30050e9434c45d6aebe6ce3b
Paramveersinh-Rathod <paramvirrathod12@gmail.com>
2026-06-03 14:09:22 +0530

    01-folder done

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <div>
+        <h1> hi </h1>
+        <h3> heyhey</h3>
+      </div>
+      <div>
+        <h1> hi </h1>
+        <h3> heyhey</h3>
+      </div>
+    </>
+
+  )
+}
+
+export default App
